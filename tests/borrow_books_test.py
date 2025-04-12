c01eae867db14b24c9bee11942524bbd69da34bf
Muhtar <Muhtar.Mahmut0519@gmail.com>
2025-04-11 20:53:10 -0400

    Initial Commit

--- tests/borrow_books_test.py
+++ tests/borrow_books_test.py
@@ -4,10 +4,9 @@ from pages.borrow_books_page import BorrowBooksPage
 
 
 def test_borrow_books_link(driver):
-    borrow_books_page = BorrowBooksPage()
-    assert borrow_books_page.borrow_books_link.is_displayed(), "Borrowing Books link is not visible"
-    assert borrow_books_page.borrow_books_link.is_enabled(), "Borrowing Books link is not enabled"
-    borrow_books_page.borrow_books_link.click()
+    assert BorrowBooksPage().borrow_books_link.is_displayed(), "Borrowing Books link is not visible"
+    assert BorrowBooksPage().borrow_books_link.is_enabled(), "Borrowing Books link is not enabled"
+    BorrowBooksPage().borrow_books_link.click()
 
 
 """
